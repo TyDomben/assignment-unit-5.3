d2e1b29be0871086d3d4fc5d9f2b384bca83449c
Ty Domben <ty.domben@gmail.com>
2023-11-26 10:48:37 -0600

    code is working

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -9,24 +9,43 @@ function addToCollection(collection, title, artist, yearPublished) {
   return album;
 }
 
+function showCollection(collection) {
+  for (let album of collection) {
+    console.log(`${album.title} by ${album.artist}, published in ${album.yearPublished}`);
+  }
+}
 
 let album0 = addToCollection(myCollection, 'Shaved Headers', 'Britney Spears', 2007);
+console.log(album0);
+
 let album1 = addToCollection(myCollection, 'Trapped in the Closet', 'R. Kelly', 2005);
+console.log(album1);
+
 let album2 = addToCollection(myCollection, 'Dating Travis Kelse', 'Taylor Swift', 2023);
+console.log(album2);
+
 let album3 = addToCollection(myCollection, 'Merry Christmas', 'Mariah Carey', 2020);
+console.log(album3);
+
 let album4 = addToCollection(myCollection, 'The Pick of Destiny', 'Tenacious D', 2006);
+console.log(album4);
+
 let album5 = addToCollection(myCollection, 'Sandstorm', 'Darude', 2009);
+console.log(album5);
 
+let album6 = addToCollection(myCollection, 'Toxic', 'Britney Spears', 2007);
+console.log(album6);
 
-//addToCollection(myCollection, 'Shaved Headers', 'Britney Spears', 2007);
-//let album0 = addToCollection(myCollection, 'Shaved Headers', 'Britney Spears', 2007);
+console.log(myCollection);
 
-console.log (album0);
-console.log (album1);
-console.log (album2);
-console.log (album3);
-console.log (album4);
-console.log (album5);
+function findByArtist(collection, artist) {
+  return collection.filter(album => album.artist === artist);
+}
+
+// Test findByArtist
+console.log(findByArtist(myCollection, 'Britney Spears')); // Should return albums by Britney Spears
+console.log(findByArtist(myCollection, 'Nonexistent Artist')); // Should return an empty array
+
 
 
 
